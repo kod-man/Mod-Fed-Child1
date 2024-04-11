93f47960e550764fefb7a9ca60e3c37d6e8af6eb
kodman <ufuk.kodman@gmail.com>
2024-04-11 14:40:51 +0200

    module federation setting up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MyButton from "parent/MyButton";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Altta parent appdan gelen componenti goreceksiniz</p>
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "child1",
+      remotes: {
+        parent: "http://localhost:5173/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
